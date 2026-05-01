62b90f159d8849d3978afc663c90ed8e007f3520
Asefa Kidanu <asefakidanu21@gmail.com>
2026-05-01 09:02:24 +0300

    second

--- app.js
+++ app.js
@@ -12,5 +12,12 @@ export function divide(a, b) {
     return a / b;
 }
 
+let a = 10;
+let b = 5;
+console.log(add(a, b));
+console.log(subtract(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
+
   
 
