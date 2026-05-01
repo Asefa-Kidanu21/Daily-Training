f3f1fa892e6bac183bb846f447be379720212bd2
Asefa Kidanu <asefakidanu21@gmail.com>
2026-05-01 08:59:10 +0300

    updated scripts

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+let __filename =fileURLToPath(import.meta.url);
+let __dirname=path.dirname(__filename);
+
+
+console.log(__dirname);
+console.log(__filename);
+
+
+
